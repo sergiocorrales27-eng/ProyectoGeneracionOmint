7707eb30d4db4984f5f1326a7d60922c9168059c
Celes <celestemaggi2006@gmail.com>
2026-02-22 20:54:36 -0300

    agrego capa models y objeto persona y usuario

---
+++ ProyectoGeneracionOmint/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoGeneracionOmint.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int UserId { get; set; }
+        public User() { }
+        public User(string username, string password, int userId)
+        {
+            Username = username;
+            Password = password;
+            UserId = userId;
+        }
+    }
+}
